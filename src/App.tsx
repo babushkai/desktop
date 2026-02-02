37a9b8f8bc5844a94977607af7e5c7fa4f2025c1
kwdaisuke <daisuke0582@gmail.com>
2026-02-02 09:13:52 +0100

    fix: add min-h-0 to allow canvas section to shrink during resize

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ function App() {
           onToggleOutputPanel={toggleOutputPanel}
         />
 
-        <div className="flex flex-1 overflow-hidden">
+        <div className="flex flex-1 min-h-0 overflow-hidden">
           {/* Node Palette - collapsible */}
           <div
             className={cn(
